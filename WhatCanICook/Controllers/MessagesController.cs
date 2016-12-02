06ecd7fb08d958c15b17c91f0d8cd26833dbec07
Rubens Koji Miura <rubenskm@yahoo.com.br>
2016-12-02 17:56:46 -0200

    alteracao

--- WhatCanICook/Controllers/MessagesController.cs
+++ WhatCanICook/Controllers/MessagesController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
 
 namespace WhatCanICook
 {
@@ -36,7 +33,7 @@ namespace WhatCanICook
                 if (activity.Text.Equals("reset", StringComparison.CurrentCultureIgnoreCase))
                 {
                     MessagesController.SetInternalState(CookBotState.Intro);
-                    MessageIntro.status = 0;
+                    messageIntro = new MessageIntro();
                 }
                 else
                 {
@@ -49,10 +46,10 @@ namespace WhatCanICook
                             await MessageIngredientes.Post(activity);
                             break;
                         case CookBotState.Webapi:
-                            await MessageWebapi.Post(activity);
+                            await messageWebApi.Post(activity);
                             break;
                         case CookBotState.Intro:
-                            await MessageIntro.Post(activity);
+                            await messageIntro.Post(activity);
                             break;
                     }
                 }
